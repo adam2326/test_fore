5bdd10ea90f4100bc4a6b84bf68a43b2a681be01
adam2326 <adampilz.2326@gmail.com>
2017-04-06 19:10:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	url = 'https://www.quandl.com/api/v3/datasets/WIKI/bbt.json?&start_date=2017-03-07&end_date=2017-04-06&column_index=4api_key=AFL4TBHtwc8xcD3ezLDy'
+	url = 'https://www.quandl.com/api/v3/datasets/WIKI/GOOG.json?&start_date=2017-03-07&end_date=2017-04-06&column_index=4api_key=AFL4TBHtwc8xcD3ezLDy'
 	response = urlfetch.fetch(url, validate_certificate=true)
 	if responce.status_code == 200:
 		return "<html><body> {} </body></html>".format( response.content )
