bcd208b372c945e23254a16f1ea32f38b383e1ce
adam2326 <adampilz.2326@gmail.com>
2017-04-06 19:08:44 -0400

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,19 @@
+# my imports
+from flask import Flask
+#from datetime import datetime, timedelta
+
+# GAE imports
+from google.appengine.api import urlfetch
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	url = 'https://www.quandl.com/api/v3/datasets/WIKI/bbt.json?&start_date=2017-03-07&end_date=2017-04-06&column_index=4api_key=AFL4TBHtwc8xcD3ezLDy'
+	response = urlfetch.fetch(url, validate_certificate=true)
+	if responce.status_code == 200:
+		return "<html><body> {} </body></html>".format( response.content )
+	else:
+		return "<html><body> {} </body></html>".format( "fuck")
+
+
